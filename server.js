11a0b040ba488a3a5cc218a91ab2977cfeede3d4
fjorn-x <hamzashaikh00@gmail.com>
2022-09-08 11:19:07 +0530

    Completed Assignment

---
+++ server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+app.listen(3000, () => console.log(" Server Started "));
+//Routes
+
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log(" Connected to Database "));
+app.use(express.json());
+
+//Routes
+
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
